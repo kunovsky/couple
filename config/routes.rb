30ff709d6b5e7c75d6fb51a77eb371dbb1ccc38d
Tyson Kunovsky <tyson@gottman.com>
2014-08-30 21:23:15 -0500

    cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  root to: 'application#index'
-
-  get '/questionnaire' => 'application#index'
-  get '/questionnaire/*url' => 'application#index'
-  get '/results' => 'application#index'
-  get '/logout' => 'sessions#destroy'
-
-  resources :sessions
-  resources :users do
-    resources :actual_responses
-    resources :completed_questionnaires
-  end
 
   scope 'api' do
+    get '/questionnaires/:id' => 'questionnaires#show'
+
+    resources :users do
+      post 'completed_questionnaires' => 'completed_questionnaires#create'
+      post 'actual_responses' => 'actual_responses#create'
+    end
+
     resources :relationships do
     post '/score' => 'relationships#score'
     get '/results/:id' => 'relationships#results'
     end
-    resources :questionnaires do
-      resources :questions do
-      end
-    end
   end
+
+  resources :sessions
+
+  get '/questionnaire' => 'application#index'
+  get '/questionnaire/*url' => 'application#index'
+  get '/results' => 'application#index'
+
+  get '/logout' => 'sessions#destroy'
+
+  root to: 'application#index'
+
 end
