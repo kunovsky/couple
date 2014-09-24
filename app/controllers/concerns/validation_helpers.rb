906eb79ad023f38d4addb0ae973e1ace7920079b
Tyson Kunovsky <tyson@gottman.com>
2014-09-23 21:21:53 -0500

    refactoring validations

--- app/controllers/concerns/validation_helpers.rb
+++ app/controllers/concerns/validation_helpers.rb
@@ -1,19 +1,18 @@
 module ValidationHelpers
     #TODO: Implement these
-
-  def valid_params(params)
-    valid_number?(params.fetch(:number, nil)) || valid_email?(params.fetch(:email, nil))
+  def valid_params?(params)
+    valid_number?(params.fetch(:text, nil)) || valid_email?(params.fetch(:email, nil))
   end
 
   def valid_number?(number) #TODO: figure out how we do this on the front end for real
-    return unless number
+    return unless number.present?
     number.gsub!("-", "")
     number.gsub!("/", "")
     number.length == 10
   end
 
   def valid_email?(email)
-    return unless email
+    return unless email.present?
     email
   end
 end
