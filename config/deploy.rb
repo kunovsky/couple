c585c0dfc89f8c76aad6c728f4cc522b7cb8428b
Tyson Kunovsky <tyson@gottman.com>
2014-10-05 18:31:34 -0500

    still testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:kunovsky/couple.git"
-set :branch, "f/deploy"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
