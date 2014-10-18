87943c9ec0f29cb5e60f657e99a8ec2c07e7761c
Tyson Kunovsky <tyson@gottman.com>
2014-10-18 13:38:50 -0500

    minor

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,5 +1,5 @@
 class GroupingsController < ApplicationController
-  include QuestionnaireHelpers
+  include GroupingHelpers
   def show
     render json: select_and_format_all_questionnaires(params[:id])
   end
