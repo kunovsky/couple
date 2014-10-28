22107c5077d07de39b3e1478ed065bef1dc72282
Tyson Kunovsky <tyson@gottman.com>
2014-10-28 07:08:56 -0500

    wip

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,12 @@
 class PurchasesController < ApplicationController
   def create
     #TODO: Implement the integration with stripe
-    render json: {success: true}, status: 200
+    render json: order_number, status: 200
+  end
+
+  private
+
+  def order_number
+    123456789
   end
 end
